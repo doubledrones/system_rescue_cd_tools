26ef6f82c19d41213cb29c3707f869c68ee8c7bd
Marcin Nowicki <pr0d1r2@gmail.com>
2014-01-13 20:23:45 +0100

    Add install of vbetool to blank screen when installing.

---
+++ install-vbetool.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+D_R=`cd \`dirname $0\` ; pwd`
+
+case "`which sudo`" in
+  "")
+    if [ ! -f functions.sh ]; then
+      curl -L https://raw.github.com/doubledrones/system_rescue_cd_tools/master/functions.sh -o $D_R/functions.sh
+    fi
+    source $D_R/functions.sh
+
+    OTHER="
+    eclass
+    "
+
+    DEPENDENCIES="
+    sys-devel/autoconf-wrapper
+    sys-devel/automake-wrapper
+    sys-devel/gettext
+    sys-devel/autoconf
+    dev-perl/Locale-gettext
+    sys-apps/help2man
+    sys-devel/automake
+    "
+
+    EBUILDS="
+    sys-apps/vbetool
+    "
+
+    portage_part_sync $OTHER $DEPENDENCIES $EBUILDS
+
+    portage_emerge $EBUILDS
+    portage_cleanout $OTHER $DEPENDENCIES $EBUILDS
+    ;;
+esac
