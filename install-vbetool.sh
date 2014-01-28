6e8d312b34810d9bf522d519eabf3adcc284cdc2
Marcin Nowicki <pr0d1r2@gmail.com>
2014-01-28 16:42:31 +0100

    Simplify syntax.

--- install-vbetool.sh
+++ install-vbetool.sh
@@ -2,34 +2,32 @@
 
 D_R=`cd \`dirname $0\` ; pwd`
 
-case "`which vbetool`" in
-  "")
-    if [ ! -f functions.sh ]; then
-      curl -L https://raw.github.com/doubledrones/system_rescue_cd_tools/master/functions.sh -o $D_R/functions.sh
-    fi
-    source $D_R/functions.sh
+which vbetool 1>/dev/null 2>/dev/null && exit 0
 
-    OTHER="
-    eclass
-    "
+if [ ! -f functions.sh ]; then
+  curl -L https://raw.github.com/doubledrones/system_rescue_cd_tools/master/functions.sh -o $D_R/functions.sh
+fi
+source $D_R/functions.sh
 
-    DEPENDENCIES="
-    sys-devel/autoconf-wrapper
-    sys-devel/automake-wrapper
-    sys-devel/gettext
-    sys-devel/autoconf
-    dev-perl/Locale-gettext
-    sys-apps/help2man
-    sys-devel/automake
-    "
+OTHER="
+eclass
+"
 
-    EBUILDS="
-    sys-apps/vbetool
-    "
+DEPENDENCIES="
+sys-devel/autoconf-wrapper
+sys-devel/automake-wrapper
+sys-devel/gettext
+sys-devel/autoconf
+dev-perl/Locale-gettext
+sys-apps/help2man
+sys-devel/automake
+"
 
-    portage_part_sync $OTHER $DEPENDENCIES $EBUILDS
+EBUILDS="
+sys-apps/vbetool
+"
 
-    portage_emerge $EBUILDS
-    portage_cleanout $OTHER $DEPENDENCIES $EBUILDS
-    ;;
-esac
+portage_part_sync $OTHER $DEPENDENCIES $EBUILDS
+
+portage_emerge $EBUILDS
+portage_cleanout $OTHER $DEPENDENCIES $EBUILDS
