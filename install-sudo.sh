8e3890ac4b100b0afc1f5259d058069855624817
Marcin Nowicki <pr0d1r2@gmail.com>
2014-01-28 16:43:16 +0100

    Simplify syntax.

--- install-sudo.sh
+++ install-sudo.sh
@@ -2,33 +2,31 @@
 
 D_R=`cd \`dirname $0\` ; pwd`
 
-case "`which sudo`" in
-  "")
-    if [ ! -f functions.sh ]; then
-      curl -L https://raw.github.com/doubledrones/system_rescue_cd_tools/master/functions.sh -o $D_R/functions.sh
-    fi
-    source $D_R/functions.sh
+which sudo 1>/dev/null 2>/dev/null && exit 0
 
-    OTHER="
-    eclass
-    "
+if [ ! -f functions.sh ]; then
+  curl -L https://raw.github.com/doubledrones/system_rescue_cd_tools/master/functions.sh -o $D_R/functions.sh
+fi
+source $D_R/functions.sh
 
-    DEPENDENCIES="
-    sys-devel/bison
-    net-mail/mailbase
-    mail-mta/ssmtp
-    "
+OTHER="
+eclass
+"
 
-    EBUILDS="
-    app-admin/sudo
-    "
+DEPENDENCIES="
+sys-devel/bison
+net-mail/mailbase
+mail-mta/ssmtp
+"
 
-    portage_part_sync $OTHER $DEPENDENCIES $EBUILDS
+EBUILDS="
+app-admin/sudo
+"
 
-    echo "sys-devel/bison -nls" >> /etc/portage/package.use
-    echo "mail-mta/ssmtp -ipv6 mta -ssl" >> /etc/portage/package.use
+portage_part_sync $OTHER $DEPENDENCIES $EBUILDS
 
-    portage_emerge $EBUILDS
-    portage_cleanout $OTHER $DEPENDENCIES $EBUILDS
-    ;;
-esac
+echo "sys-devel/bison -nls" >> /etc/portage/package.use
+echo "mail-mta/ssmtp -ipv6 mta -ssl" >> /etc/portage/package.use
+
+portage_emerge $EBUILDS
+portage_cleanout $OTHER $DEPENDENCIES $EBUILDS
